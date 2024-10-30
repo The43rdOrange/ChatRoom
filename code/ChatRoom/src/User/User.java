d013875fe80140926e87b539bf4ba6fa038955b8
Daniel Cook <danielcook2102@gmail.com>
2024-10-30 19:48:45 +0000

    Fixing the bug with the threads.

--- code/ChatRoom/src/User/User.java
+++ code/ChatRoom/src/User/User.java
@@ -15,54 +15,76 @@ public class User {
     final List<Message> inbox = new ArrayList<>();
     final List<Message> outbox = new ArrayList<>();
     UserConnectionToHost userConnectionToHost;
+    Scanner scanner = new Scanner(System.in);
 
-    Thread inboxThread = new Thread(() -> {
-        synchronized (inbox){
-            Message msg;
-            while (!inbox.isEmpty()) {
-                msg = inbox.getFirst();
-                inbox.remove(msg);
-                System.out.println(msg.toDisplayableString());
+    Thread getInboxThread(){
+        return new Thread(() -> {
+            synchronized (inbox){
+                Message msg;
+                while (!inbox.isEmpty()) {
+                    msg = inbox.getFirst();
+                    inbox.remove(msg);
+                    System.out.println(msg.toDisplayableString());
+                }
             }
-        }
-    });
-    Thread getUserInputThread = new Thread(() -> {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
+        });
+    }
+    Thread getUserInputThread(){
+        return new Thread(() -> {
+            System.out.println("Awaiting user input");
+            String s = scanner.nextLine();
+            System.out.println("User input was " + s);
 
 
-        synchronized (outbox) {
-            outbox.add(new TextMessage(this.username.get(),s));
-        }
-    });
-    Thread outboxThread = new Thread(() -> {
-        synchronized (outbox) {
-            while (!outbox.isEmpty()) {
-                userConnectionToHost.addToOutbox(outbox.removeFirst());
+            synchronized (outbox) {
+                outbox.add(new TextMessage(this.username.get(),s));
             }
-        }
-    });
+        });
+    }
+    Thread getOutboxThread(){
+        return new Thread(() -> {
+            synchronized (outbox) {
+                while (!outbox.isEmpty()) {
+                    userConnectionToHost.addToOutbox(outbox.removeFirst());
+                }
+            }
+        });
+    }
 
     public User() {
         //temp username that will almost certainly be unique for now
         username.set(LocalDateTime.now().toString());
     }
 
+    /**
+     * The loop that contains all the logic for the user when it comes to sending/receiving data
+     */
     public void loop() {
         boolean continueLoop = true;
+        Thread getUserInput = null;
+        Thread handleOutbox = null;
+        Thread handleInbox = null;
 
-        //start threads if they aren't already running
+        //In java, threads can only be started once. This leads to the bizarre code below
+        //The below code, checks if the variable is null, if it is, the variable  will be
+        // set. If the thread has finished executing, the thread will be set again as
+        // threads can only be started once with the start function. The thread is then
+        // started
         while (continueLoop) {
-            if (!inboxThread.isAlive()) {
-                inboxThread.start();
+
+            if (getUserInput == null || !getUserInput.isAlive()) {
+                getUserInput = getUserInputThread();
+                getUserInput.start();
             }
 
-            if (!outboxThread.isAlive()) {
-                outboxThread.start();
+            if (handleOutbox == null || !handleOutbox.isAlive()) {
+                handleOutbox = getOutboxThread();
+                handleOutbox.start();
             }
 
-            if (!getUserInputThread.isAlive()) {
-                getUserInputThread.start();
+            if (handleInbox == null || !handleInbox.isAlive()) {
+                handleInbox = getInboxThread();
+                handleInbox.start();
             }
         }
     }
